a2d51efb66b6f89a455ea734d9d2770741057574
ramses2 <paul.levent@sciencespo.fr>
2019-05-26 19:37:27 +0200

    inital commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function convert (input) {
+  let result = 0;
+  let userCelsius = document.getElementById("C").value
+  let userFahrenheit = document.getElementById("F").value
+
+  if (userCelsius) {
+    result = userCelsius*(9/5) + 32;
+    document.getElementById("F").value = result;
+  } else {
+    result = userFahrenheit*(9/5) + 32;
+    document.getElementById("C").value = result;
+  }
+}
