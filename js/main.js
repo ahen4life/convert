2c626ec4d525a0771aedc6d930699726fb2b0f54
ramses2 <paul.levent@sciencespo.fr>
2019-05-26 22:53:19 +0200

    edited files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 function convert (input) {
   let result = 0;
-  let userCelsius = document.getElementById("C").value
-  let userFahrenheit = document.getElementById("F").value
+  let userCelsius = document.getElementById("C").value;
+  let userFahrenheit = document.getElementById("F").value;
 
   if (userCelsius) {
-    result = userCelsius*(9/5) + 32;
+    result = Math.round(userCelsius*(9/5) + 32);
     document.getElementById("F").value = result;
   } else {
     result = userFahrenheit*(9/5) + 32;
